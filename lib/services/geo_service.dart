803246d58ef503bfc2e11c8be8570a33a0a2eb69
Florian Sacchetti <florian.sacchetti@gmail.com>
2024-03-17 17:50:10 +0900

    bug fix

--- lib/services/geo_service.dart
+++ lib/services/geo_service.dart
@@ -19,7 +19,7 @@ class GeoService extends GlobalService {
 
   Future<Forecast?> fetchCurrentWeather(Coord coordinates) async {
     final response = await httpRequest(
-        HttpMethod.get, ApiVersion.v30, "onecall",
+        HttpMethod.get, ApiVersion.v30, "/onecall",
         queryParameters: {
           "lat": coordinates.lat,
           "long": coordinates.long,
