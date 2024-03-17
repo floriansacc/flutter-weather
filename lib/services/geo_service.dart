22cf0c445c7883788e2d59e3875fafb62ffed7c2
Maël Le Goff <legoffmael@gmail.com>
2024-03-17 17:34:25 +0900

    ✨ get device coordinates + display it

--- lib/services/geo_service.dart
+++ lib/services/geo_service.dart
@@ -2,9 +2,21 @@ import 'dart:convert';
 
 import 'package:flutter_weather/models/coordinates.dart';
 import 'package:flutter_weather/models/forecast.dart';
+import 'package:flutter_weather/services/geo_locator.dart';
 import 'package:flutter_weather/services/global_service.dart';
+import 'package:flutter_weather/shared/utils/logger.dart';
 
 class GeoService extends GlobalService {
+  Future<Forecast?> fetchCurrentCoordWeather() async {
+    try {
+      final coord = await GeoLocator().getCoordinates();
+      return fetchCurrentWeather(coord);
+    } catch (e, s) {
+      errorLog(e, s);
+      return null;
+    }
+  }
+
   Future<Forecast?> fetchCurrentWeather(Coord coordinates) async {
     final response = await httpRequest(
         HttpMethod.get, ApiVersion.v30, "onecall",
